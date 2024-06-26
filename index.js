50e471154cdbcd0aa1087a5dfaf1ae5f8527dd35
Abdikhafar <abdikhafarissack@gmail.com>
2024-06-26 23:02:07 +0300

    phase 1

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = 'Abdikhafar';
+ const welcomeMessage = ' Welcome to Flatbook, Abdikhafar!';
+ const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ABDIKHAFAR!';
+ const shortGreeting = 'Welcome, A!';
